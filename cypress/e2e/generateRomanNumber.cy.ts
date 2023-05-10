eca7ab643b6ecaeafc29892e131c86c2d2ed949d
garamvolgyisoma <garamvolgyi.soma@gmail.com>
2023-05-10 11:45:02 +0200

    project init

---
+++ cypress/e2e/generateRomanNumber.cy.ts
@@ -0,0 +1,7 @@
+import Cypress from 'cypress';
+
+describe('E2E test', () => {
+  it('passes', () => {
+    cy.visit('/')
+  })
+})
---
+++ src/intToRoman.ts
@@ -0,0 +1,5 @@
+const intToRoman = () => {
+
+}
+
+export default intToRoman;
