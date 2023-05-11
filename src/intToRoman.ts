14f644cbe647a0270c5f842844596a03c035c2f1
garamvolgyisoma <garamvolgyi.soma@gmail.com>
2023-05-11 10:06:24 +0200

    Implement converting functionality

--- src/intToRoman.ts
+++ src/intToRoman.ts
@@ -1,5 +1,21 @@
-const intToRoman = () => {
+const intToRoman = (input:number):string => {
+    let nums:number[] = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    let symbols:string[] = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+    let i = 12;
 
+    let romanNum:string = "";
+    
+    while(input > 0) {
+        let div = Math.floor(input/nums[i]);
+        input = input%nums[i];
+        
+        while(div--) {
+            romanNum += symbols[i];
+        }
+
+        i--;
+    }
+    return romanNum;
 }
 
 export default intToRoman;
