eb2ba2478f6073c206ad0be55f2d6e41370fd558
garamvolgyisoma <garamvolgyi.soma@gmail.com>
2023-05-11 10:05:50 +0200

    Define unit test

--- src/intToRoman.test.ts
+++ src/intToRoman.test.ts
@@ -1,1 +1,18 @@
 import intToRoman from './intToRoman';
+
+describe('integer to roman number converter', () => {
+    test('returns correct roman number for 10', () => {
+        const resultOne = intToRoman(10);
+        expect(resultOne).toBe("X");
+    });
+
+    test('returns correct roman number for 1514', () => {
+        const resultTwo = intToRoman(1514);
+        expect(resultTwo).toBe("MDXIV");
+    });
+
+    test('returns correct roman number for 1514', () => {
+        const resultThree = intToRoman(3753);
+        expect(resultThree).toBe("MMMDCCLIII");
+    });
+});
